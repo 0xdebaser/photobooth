f16979681ff0b4d3349ab9fdd9c7fa1b8eb1dde8
0xdebaser <0xdebaser@gmail.com>
2022-06-28 07:21:48 -1000

    early commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <nav class="navbar" id="nav-main">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1 text-brand">fizzgen</span>
+        </div>
+      </nav>
+    </div>
+  );
+}
+export default Navbar;
