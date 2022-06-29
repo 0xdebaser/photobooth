8b4e986644595e85572fdd6126461545ffda7a4d
0xdebaser <0xdebaser@gmail.com>
2022-06-28 16:27:14 -1000

    early commits

--- src/components/CamButton.jsx
+++ src/components/CamButton.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function CamButton(props) {
-  return <div>Button</div>;
+  return (
+    <div className="col text-center">
+      {props.primary && (
+        <button className="btn btn-cam btn-secondary" onClick={props.handler}>
+          {props.label}
+        </button>
+      )}
+      {!props.primary && (
+        <button className="btn btn-cam btn-primary" onClick={props.handler}>
+          {props.label}
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default CamButton;
--- src/components/WebcamSuite.jsx
+++ src/components/WebcamSuite.jsx
@@ -3,22 +3,51 @@ import Webcam from "react-webcam";
 import CamButton from "./CamButton";
 
 function WebcamSuite() {
-  const [imgCapture, setImgCaptured] = useState(false);
+  const webcamRef = React.useRef(null);
+  const [imgSrc, setImgSrc] = useState(null);
+  const [imgCaptured, setImgCaptured] = useState(false);
   const videoConstraints = {
     width: 360,
     height: 360,
     facingMode: "user",
   };
 
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot({
+      width: 360,
+      height: 360,
+    });
+    setImgSrc(imageSrc);
+    setImgCaptured(true);
+  }, [webcamRef, setImgSrc]);
+
+  function reset() {
+    setImgSrc(null);
+    setImgCaptured(false);
+  }
+
   return (
-    <div className="container mt-3">
+    <div className="container mt-2">
       <div className="row">
-        <Webcam
-          audio={false}
-          height={360}
-          width={360}
-          videoConstraints={videoConstraints}
-        />
+        {!imgCaptured && (
+          <Webcam
+            audio={false}
+            ref={webcamRef}
+            height={360}
+            width={360}
+            videoConstraints={videoConstraints}
+            screenshotFormat="image/jpeg"
+          />
+        )}
+        {imgCaptured && (
+          <img className="img-captured center-block" src={imgSrc} />
+        )}
+      </div>
+      <div className="row mt-2">
+        {!imgCaptured && (
+          <CamButton label="capture" handler={capture} primary={true} />
+        )}
+        {imgCaptured && <CamButton label="reset" handler={reset} />}
       </div>
     </div>
   );
